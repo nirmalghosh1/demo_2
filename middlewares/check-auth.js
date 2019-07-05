af74f903827276a3ebb4acf3eef81d3bc6691ee9
Nirmal Ghosh <nirmal@newage.local>
2019-07-05 14:06:21 +0530

    change made to the auth.js again

--- middlewares/check-auth.js
+++ middlewares/check-auth.js
@@ -7,7 +7,7 @@ module.exports = (req, res, next) => function() {
         next();
     } catch(error) {
         res.status(401).json({
-            message: 'You are not authenticated,.'
+            message: 'You are not authenticated and you are blocked.'
         });
     } 
 };
