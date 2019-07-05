ed67a1104f45d9cb845b71fd82dc1dab1ef42faa
Nirmal Ghosh <nirmal@newage.local>
2019-07-05 14:10:50 +0530

    Middle ware

--- middlewares/check-auth.js
+++ middlewares/check-auth.js
@@ -7,7 +7,7 @@ module.exports = (req, res, next) => function() {
         next();
     } catch(error) {
         res.status(401).json({
-            message: 'You are not authenticated and you are blocked for ever.'
+            message: 'You are not authenticated and you are block.'
         });
     } 
 };
